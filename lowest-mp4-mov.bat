5cebffe672883508f9638c3b6baf2c0b5bdaf0de
mlclmtan <mlclmtan@gmail.com>
2020-09-11 20:09:18 +0800

    auto create folder

--- lowest-mp4-mov.bat
+++ lowest-mp4-mov.bat
@@ -1,3 +1,5 @@
-FOR /F "tokens=*" %%G IN ('dir /b *.mp4') DO ffmpeg -i "%%G" -c:v libx264 -c:a copy -crf 28 -vf scale=-1:720 "output\%%~nG.mp4"
+if NOT EXIST "compressed\" MKDIR "compressed\"
 
-FOR /F "tokens=*" %%G IN ('dir /b *.mov') DO ffmpeg -i "%%G" -c:v libx264 -c:a copy -crf 28 -vf scale=-1:720 "output\%%~nG.mov"
+FOR /F "tokens=*" %%G IN ('dir /b *.mp4') DO ffmpeg -i "%%G" -c:v libx264 -c:a copy -crf 28 -vf scale=-1:720 compressed\%%~nG.mp4"
+
+FOR /F "tokens=*" %%G IN ('dir /b *.mov') DO ffmpeg -i "%%G" -c:v libx264 -c:a copy -crf 28 -vf scale=-1:720 compressed\%%~nG.mov"
